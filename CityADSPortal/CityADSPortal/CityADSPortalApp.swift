5343c6ad0ec7c027afacd3ef66dbbbd34dc2b2a6
imChil <ChilMining@gmail.com>
2022-12-13 19:59:59 +0300

    Init
    initialization

---
+++ CityADSPortal/CityADSPortal/CityADSPortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityADSPortalApp.swift
+//  CityADSPortal
+//
+//  Created by  Pavel Chilin on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CityADSPortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
